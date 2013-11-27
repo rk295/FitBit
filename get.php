e5eb712e7f39bc9414dcf6b995d8dc2a452223f7
Robin Kearney <robin@kearney.co.uk>
2013-11-27 21:09:53 +0000

    Tidied up a bit!

--- get.php
+++ get.php
@@ -1,4 +1,19 @@
 <?php
+// 
+// get.php
+// -------
+// 
+// Grabs the array from memcache, (currently just _assumes_ it is there)
+// and returns a json array of timestamp,steps. 
+// 
+// Also takes a period parameter to specify only day in a given period, 
+// i.e. 8d, 6m etc. 
+// 
+// Currently _only_ supports steps, and has almost not error checking, 
+// for example will die silently if the data isn't in memcache.
+// 
+
+include('conf.php');
 
 $memcache_obj = memcache_connect('localhost', 11211);
 
@@ -6,7 +21,7 @@ $allData = $memcache_obj->get('fitbit-data');
 
 $output = array();
 
-$havePeriod = false;
+$savedThings = array();
 
 if ( isset($_GET['period']) && $_GET['period'] != "" && $_GET['period'] != "all" ){
 
@@ -22,21 +37,20 @@ if ( isset($_GET['period']) && $_GET['period'] != "" && $_GET['period'] != "all"
 	// http://php.net/manual/en/dateinterval.construct.php
 	$then->sub(new DateInterval($period));
 
-	$havePeriod = true;
 }
 
+$output['data'][0]['legend']  = "Steps";
+$output['data'][1]['legend']  = "Floors";
 
 foreach( $allData as $timestamp => $data){
 
 	$dt = new DateTime("@$timestamp");
 
-	if ( $havePeriod ){
-		if ( $dt > $then ) {
-			$output[$timestamp] = $data["steps"];
-		}
-	}else{
-		$output[$timestamp] = $data["steps"];
-	}
+	if ( $dt < $then ){ continue; }
+
+	
+	$output['data'][0]['data'][$timestamp]  = $data["steps"];
+	$output['data'][1]['data'][$timestamp] = $data["floors"];
          
 }
 
--- read.php
+++ read.php
@@ -1,30 +1,55 @@
 <?php
+// 
+// read.php
+// --------
 
-$memcache_obj = memcache_connect('localhost', 11211);
+// Reads all the files in the backupdir specified in conf.php and stores
+// all the summary fields for each day in an array in memcache. Intended 
+// to be called daily after fetchAct.php is run, the array it stores can
+// be used by get.php
+// 
 
-$dir="/home/robin/Dropbox/Backups/fitbit/dailyactivities";
+include('conf.php');
+
+
+$mcHost = 'localhost';
+$mcPort = 11211;
+
+if ( ! $memcache_obj = memcache_connect($mcHost,$mcPort) ){
+	print "Failed to connect to memcache ($mcHost:$mcPort)";
+	exit;
+}
 
 $allData = array();
+$counter = 0;
 
-if ($handle = opendir($dir)) {
+if ($handle = opendir($backupDir)) {
 
     while (false !== ($entry = readdir($handle))) {
         if ( $entry == "." || $entry == ".." ) { continue; }
-
 
         $date_str = preg_replace('/-summary.json/','',$entry);
 
         $dt = new DateTime($date_str); 
         $timeStamp = $dt->format('U');
 
-        $data = json_decode(file_get_contents($dir . "/" . $entry),true);
+        $data = json_decode(file_get_contents($backupDir . "/" . $entry),true);
 
         $allData[$timeStamp] = $data["summary"];
 
+        $counter++;
+
     }
     closedir($handle);
+    
+}else{
+	print "Failed to opendir $backupDir\n";
+	exit;
 }
 
 $memcache_obj->set('fitbit-data', $allData);
+
+print "Saved $counter entries into memcache";
+exit;
 
 ?>
